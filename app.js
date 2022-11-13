89428a78f59be61648f46abf6fe2616fdd489cc8
mohsinjavedcheema <86498762+mohsinjavedcheema@users.noreply.github.com>
2022-11-14 02:02:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.post("/login", async (req, res) => {
 });
 
 app.get("/welcome", auth, (req, res) => {
-  res.status(200).send("Welcome 🙌 ");
+  res.status(200).send("Hello World!");
 });
 
 // This should be the last route else any after it won't work
